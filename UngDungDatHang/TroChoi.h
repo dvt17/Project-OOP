76fcebd2ee3044b3e24947a0ba24a831ee38c477
SPykeX-532 <anhtaiminecraft@gmail.com>
2025-11-08 03:41:36 +0700

    Phan Anh Tai commit lan 3

--- UngDungDatHang/TroChoi.h
+++ UngDungDatHang/TroChoi.h
@@ -1,10 +1,6 @@
 ﻿#pragma once
-#include <iostream>
-#include <string>
-#include <ctime>
 #include <cstdlib>
 #include "MaGiam.h"
-using namespace std;
 
 class TroChoi {
 private:
@@ -26,7 +22,7 @@ public:
     void BatDau() {
         cout << "=== Xin chao, " << tenNguoiChoi << " ===\n";
         cout << "Chon tro choi:\n";
-        cout << "1. Doan so\n2. Vong xoay man may\n3. Oan tu ti\n";
+        cout << "1. Doan so\n2. Vong xoay man may\n3. Quy luat day so\n";
         cout << "Nhap lua chon: ";
         cin >> chonTro;
         thoiGianBatDau = time(0);
@@ -41,7 +37,7 @@ public:
     void Game1() {
         srand(time(NULL));
         int so = rand() % 30 + 1;
-        while(luotTro1 > 0){
+        while (luotTro1 > 0) {
             int chon;
             cout << "Nhap 1 so (1-30): ";
             cin >> chon;
@@ -55,24 +51,24 @@ public:
                 cout << "So bi mat lon hon so ban chon!" << endl;
                 continue;
             }
-            else{
+            else {
                 cout << "So bi mat nho hon so ban chon!" << endl;
                 continue;
-            }           
+            }
         }
         if (luotTro1 == 0) {
-            cout << "Ban da het luoc choi!" << endl;
+            cout << "Ban da het 3 luot choi!" << endl;
         }
     }
     void Game2() {
         char dsKitu[3] = { '%','@','*' };
-        char Kitu[3], q;
+        char Kitu[3], quay;
         srand(time(NULL));
         cin.ignore();
         while (luotTro2 > 0) {
             cout << "An [Enter] de quay hoac an [X] de thoat: ";
-            cin.get(q);
-            if (q == '\n') {
+            cin.get(quay);
+            if (quay == '\n') {
                 for (int i = 0; i < 3; i++) {
                     int so = rand() % 3;
                     Kitu[i] = dsKitu[so];
@@ -93,7 +89,7 @@ public:
                 }
                 luotTro2--;
             }
-            else if (q == 'X' || q == 'x') {
+            else if (quay == 'X' || quay == 'x') {
                 break;
             }
         }
@@ -101,7 +97,92 @@ public:
             cout << "Ban da het 3 luot choi! " << endl;
         }
     }
-    void Game3(); // logic trò 3
+    void Game3() {
+        srand(time(NULL));
+        int soBatDau = rand() % 5 + 1;
+        int k = rand() % 9 + 2;
+        int quyLuat = rand() % 5 + 1;
+        int daySo[5], soDoan;
+        daySo[0] = soBatDau;
+        switch (quyLuat) {
+        case 1:
+            for (int i = 1; i < 5; i++) {
+                daySo[i] = daySo[i - 1] * k;
+            }
+            break;
+        case 2:
+            for (int i = 1; i < 5; i++) {
+                daySo[i] = daySo[i - 1] + k;
+            }
+            break;
+        case 3:
+            for (int i = 1; i < 3; i++) {
+                daySo[i] = daySo[i - 1] * 2;
+            }
+            for (int i = 3; i < 5; i++) {
+                daySo[i] = daySo[i - 1] / 2;
+            }
+            break;
+        case 4:
+            daySo[1] = daySo[0];
+            for (int i = 2; i < 5; i++) {
+                daySo[i] = daySo[i - 1] + daySo[i - 2];
+            }
+            break;
+        case 5:
+            for (int i = 1; i < 5; i++) {
+                daySo[i] = (i % 2 == 1) ? daySo[i - 1] + k : daySo[i - 1] - (k + 1);
+            }
+            break;
+        }
+        cout << "Hay tim quy luat va doan ra so tiep theo trong day so sau: " << endl;
+        for (int i = 0; i < 4; i++) {
+            if (i == 3) {
+                cout << daySo[i] << " , ?\n";
+            }
+            else {
+                cout << daySo[i] << " , ";
+            }
+        }
+        while (luotTro3 > 0) {
+            string thuLai;
+            cout << "Nhap so: ";
+            cin >> soDoan;
+            luotTro3--;
+            if (daySo[4] == soDoan) {
+                diem += 500;
+                cout << "Chinh xac! +500 diem thuong" << endl;
+                break;
+            }
+            else {
+                cout << "Ban muon thu lai khong ? (Y/N): ";
+                cin >> thuLai;
+                for (int i = 0; i < thuLai.size(); i++) {
+                    if (thuLai[i] >= 'a' && thuLai[i] <= 'z') {
+                        thuLai[i] -= 32;
+                    }
+                }
+                if (thuLai == "YES" || thuLai == "Y") {
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        if (luotTro3 == 0) {
+            cout << "Ban da het 3 luot choi! " << endl;
+            cout << "Day so: ";
+            for (int i = 0; i < 4; i++) {
+                if (i == 3) {
+                    cout << daySo[i] << endl;
+                }
+                else {
+                    cout << daySo[i] << " , ";
+                }
+            }
+        }
+    }
 
     MaGiam DoiThuong() {
         int thuong;
@@ -116,7 +197,7 @@ public:
         cout << "Chon: ";
         cin >> thuong;
         switch (thuong) {
-        case 1: 
+        case 1:
             if (diem >= 500) {
                 diem -= 500;
                 return MaGiam("FREESHIP", 100);
@@ -134,7 +215,7 @@ public:
                 cout << " So diem cua ban khong du (" << diem << " < 1000)!" << endl;
                 return MaGiam("MAGIAM0", 0);
             }
-        case 3: 
+        case 3:
             if (diem >= 1500) {
                 diem -= 1500;
                 return MaGiam("MAGIAM10", 10);
@@ -143,7 +224,7 @@ public:
                 cout << " So diem cua ban khong du (" << diem << " < 1500)!" << endl;
                 return MaGiam("MAGIAM0", 0);
             }
-        case 4: 
+        case 4:
             if (diem >= 2000) {
                 diem -= 2000;
                 return MaGiam("MAGIAM15", 15);
@@ -152,7 +233,7 @@ public:
                 cout << " So diem cua ban khong du (" << diem << " < 2000)!" << endl;
                 return MaGiam("MAGIAM0", 0);
             }
-        case 5: 
+        case 5:
             if (diem >= 2500) {
                 diem -= 2500;
                 return MaGiam("MAGIAM20", 20);
@@ -161,8 +242,7 @@ public:
                 cout << " So diem cua ban khong du (" << diem << " < 2500)!" << endl;
                 return MaGiam("MAGIAM0", 0);
             }
-        default: return MaGiam("MAGIAM0",0);
+        default: return MaGiam("MAGIAM0", 0);
         }
     }
-    ~TroChoi() {}
 };
